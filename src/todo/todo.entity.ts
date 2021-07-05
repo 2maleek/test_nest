7c859c7bd266e676f85b8e123374f7cee9313543
2maleek <222malikibrahim@gmail.com>
2021-07-05 16:06:16 +0700

    crud generator

---
+++ src/todo/todo.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class TodoEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column({
+        type: 'boolean',
+        default: false
+    })
+    is_done: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
